8447d641815e8e6ec657f4533f1802d69f2922b8
Matthew Hayes <mattjhayes3@gmail.com>
2022-05-30 16:36:27 -0700

    transformer2

---
+++ models/transformer.py
@@ -0,0 +1,86 @@
+import numpy as np
+import keras
+import keras.layers as layers
+# from common_fns import *
+from .model import SequenceModel 
+import tensorflow as tf
+import os
+import shutil
+
+class TransformerBlock(layers.Layer):
+    def __init__(self, embed_dim, num_heads, ff_dim, rate=0.1):
+        super(TransformerBlock, self).__init__()
+        self.att = layers.MultiHeadAttention(num_heads=num_heads, key_dim=embed_dim)
+        self.ffn = keras.Sequential(
+            [layers.Dense(ff_dim, activation="relu"), layers.Dense(embed_dim),]
+        )
+        self.layernorm1 = layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = layers.LayerNormalization(epsilon=1e-6)
+        self.dropout1 = layers.Dropout(rate)
+        self.dropout2 = layers.Dropout(rate)
+
+    def call(self, inputs, training):
+        attn_output = self.att(inputs, inputs)
+        attn_output = self.dropout1(attn_output, training=training)
+        out1 = self.layernorm1(inputs + attn_output)
+        ffn_output = self.ffn(out1)
+        ffn_output = self.dropout2(ffn_output, training=training)
+        return self.layernorm2(out1 + ffn_output)
+
+class TokenAndPositionEmbedding(layers.Layer):
+    def __init__(self, maxlen, vocab_size, embed_dim):
+        super(TokenAndPositionEmbedding, self).__init__()
+        self.token_emb = layers.Embedding(input_dim=vocab_size, output_dim=embed_dim)
+        self.pos_emb = layers.Embedding(input_dim=maxlen, output_dim=embed_dim)
+
+    def call(self, x):
+        maxlen = tf.shape(x)[-1]
+        positions = tf.range(start=0, limit=maxlen, delta=1)
+        positions = self.pos_emb(positions)
+        x = self.token_emb(x)
+        return x + positions
+
+class TransformerModel(SequenceModel):
+    def __init__(self, instance_name=None):
+        super().__init__(instance_name)
+        self.embedding_size = 128
+
+    def name(self):
+        return 'transformer_2_2' if not self.instance_name else f"transformer_2_2_{self.instance_name}"
+
+    # inside, save the trained model to the corresponding folder - might be needed in the future
+    def fit(self, training_matrix, training_labels, validation_matrix, validation_labels, dictionary):
+
+        training_matrix = training_matrix.toarray()
+        print(f"shape training matrix {training_matrix.shape}")
+        print("example:", training_matrix[-1, :])
+        dictionary_size = int(np.max(training_matrix) + 2)
+        print(f"dictionary_size =  {dictionary_size}")
+
+        validation_matrix = validation_matrix.toarray()
+
+        # define the early stopping criteria
+        es = keras.callbacks.EarlyStopping(monitor='val_accuracy', min_delta=0, patience=10, verbose=0, mode='auto', restore_best_weights=True)
+        self.model = keras.models.Sequential([
+            TokenAndPositionEmbedding(np.shape(training_matrix)[1], dictionary_size, self.embedding_size),
+            TransformerBlock(self.embedding_size, 2, 128),
+                                            layers.GlobalAveragePooling1D(),
+                                            layers.Dropout(0.1),
+                                            # layers.Dropout(0.5),
+                                            # layers.Conv1D(128, 7, padding="valid", activation="relu", strides=1),
+                                            # layers.GlobalMaxPooling1D(),
+                                            layers.Dense(64, activation="relu"),
+                                            # layers.Dropout(0.50),
+                                            # layers.Dropout(0.2),
+                                            keras.layers.Dense(1, activation='sigmoid'),
+                                            ])
+
+        self.model.compile(optimizer='adam', loss='binary_crossentropy',
+                        metrics=['binary_crossentropy', 'accuracy'])
+        logdir = f"./logs/{self.name()}"
+        shutil.rmtree(logdir, ignore_errors=True)
+        os.makedirs(logdir)
+        tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
+        self.model.fit(training_matrix, training_labels, epochs=200, batch_size=128,
+                    validation_data=(validation_matrix, validation_labels), callbacks=[es, tensorboard_callback])
+
